4395dd18062e9afa9bf69ecaa6f4fd9db5a24115
llach <llach@techfak.uni-bielefeld.de>
2018-04-06 11:24:58 +0200

    added fl0s object

--- core/src/object.cpp
+++ core/src/object.cpp
@@ -22,13 +22,17 @@ void spawnObject(std::string id, int primitive_type) {
     switch(primitive_type){
         case(0):
         {
-            std::cout << "spawning a box" << std::endl;
+            o.primitive_poses[0].position.x = 0.32;
+            o.primitive_poses[0].position.z = 0.856;
+            o.primitive_poses[0].orientation.w = 1.0;
+
+            std::cout << "spawning a floxx" << std::endl;
 
             o.primitives[0].type= shape_msgs::SolidPrimitive::BOX;
             o.primitives[0].dimensions.resize(3);
-            o.primitives[0].dimensions[0]= 0.15;
-            o.primitives[0].dimensions[1]= 0.23;
-            o.primitives[0].dimensions[2]= 0.10;
+            o.primitives[0].dimensions[0]= 0.273;
+            o.primitives[0].dimensions[1]= 0.355;
+            o.primitives[0].dimensions[2]= 0.055;
 
             break;
         }
@@ -43,13 +47,25 @@ void spawnObject(std::string id, int primitive_type) {
 
             break;
         }
-        default:
+        case(2):
         {
             std::cout << "spawning a sphere" << std::endl;
 
             o.primitives[0].type= shape_msgs::SolidPrimitive::SPHERE;
             o.primitives[0].dimensions.resize(1);
             o.primitives[0].dimensions[0]= 0.11;
+
+            break;
+        }
+        default: {
+
+            std::cout << "spawning a box" << std::endl;
+
+            o.primitives[0].type= shape_msgs::SolidPrimitive::BOX;
+            o.primitives[0].dimensions.resize(3);
+            o.primitives[0].dimensions[0]= 0.15;
+            o.primitives[0].dimensions[1]= 0.23;
+            o.primitives[0].dimensions[2]= 0.10;
 
             break;
         }
@@ -66,7 +82,7 @@ int main(int argc, char** argv){
     ros::NodeHandle nh_private("~");
     ros::ServiceClient client = nh.serviceClient<pepper_mtc_msgs::PepperGrasping>("pepper_grasping");
 
-    int p;
+    int p = 3;
     nh_private.getParam("primitive", p);
 
     std::string obj = "object";
