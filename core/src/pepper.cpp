3bd7e9e5fd2e31703267e1f307aae9211dcea252
Felix Friese <ffriese@techfak.uni-bielefeld.de>
2018-04-17 21:10:11 +0200

    using new task-creation, todo: cleanup

--- core/src/pepper.cpp
+++ core/src/pepper.cpp
@@ -10,6 +10,9 @@
 #include <moveit/task_constructor/stages/connect.h>
 #include <moveit/task_constructor/solvers/pipeline_planner.h>
 #include <moveit/task_constructor/solvers/cartesian_path.h>
+#include <moveit/task_constructor/stages/modify_planning_scene.h>
+#include <moveit/task_constructor/stages/connect.h>
+#include <stages/bimanual_grasp_pose.h>
 
 #include <actionlib/client/simple_action_client.h>
 #include <actionlib/client/terminal_state.h>
@@ -21,6 +24,8 @@
 #include <moveit_msgs/CollisionObject.h>
 #include <shape_msgs/SolidPrimitive.h>
 
+#include <eigen_conversions/eigen_msg.h>
+
 #include <moveit/planning_scene_interface/planning_scene_interface.h>
 #include <pepper_mtc_msgs/PepperGrasping.h>
 
@@ -30,8 +35,175 @@ using namespace moveit::task_constructor;
 // keep global reference, so service calls from rviz plugin
 // work even after the service call task trigger is done
 std::shared_ptr<Task> task_ptr_;
-std::shared_ptr<actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>> action_client_ptr;
+std::shared_ptr<actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>> l_action_client_ptr;
+std::shared_ptr<actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>> r_action_client_ptr;
 
+
+Task createTask(const std::string& object = "object") {
+    Task t;
+    t.loadRobotModel();
+
+    const std::string eef_left = "left_hand";
+    const std::string eef_right = "right_hand";
+
+    const std::string arm_left = "left_arm";
+    const std::string arm_right = "right_arm";
+
+    geometry_msgs::PoseStamped ik_frame_left;
+    ik_frame_left.header.frame_id = "l_grasp_frame";
+    tf::poseEigenToMsg(Eigen::Translation3d(0.0,0.05,0.0) *
+                       Eigen::AngleAxisd(M_PI, Eigen::Vector3d::UnitY()),
+                       ik_frame_left.pose);
+
+    geometry_msgs::PoseStamped ik_frame_right;
+    ik_frame_right.header.frame_id = "r_grasp_frame";
+    tf::poseEigenToMsg(Eigen::Affine3d(Eigen::Translation3d(0.0,0.05,0.0)),
+                       ik_frame_right.pose);
+
+
+    // cartesian planner
+    auto cartesian = std::make_shared<solvers::CartesianPath>();
+    cartesian->setProperty("jump_threshold", 0.0); // disable jump check, see MoveIt #773
+
+    // pipeline planner
+    auto pipeline = std::make_shared<solvers::PipelinePlanner>();
+    pipeline->setTimeout(8.0);
+    pipeline->setPlannerId("RRTConnectkConfigDefault");
+
+
+    Stage* referenced_stage = nullptr;
+    {  // fetch initial state from move_group
+        auto initial = new stages::CurrentState("current state");
+        t.add(std::unique_ptr<Stage>(referenced_stage = initial));
+    }
+
+    {  // connect current state to pick
+        stages::Connect::GroupPlannerVector planners = {{eef_left, pipeline}, {arm_left, pipeline},
+                                                        {eef_right, pipeline}, {arm_right, pipeline}};
+        auto connect = new stages::Connect("connect", planners);
+        connect->properties().configureInitFrom(Stage::PARENT);
+        t.add(std::unique_ptr<Stage>(connect));
+    }
+
+    auto pick = new SerialContainer("pick");
+
+    {  // approach
+        geometry_msgs::TwistStamped twist;
+        twist.twist.linear.y = 1.0;
+
+        auto merger = new Merger("approach");
+        for (const auto& eef : {eef_left, eef_right}) {
+            auto move = new stages::MoveRelative("approach " + eef, cartesian);
+            move->restrictDirection(stages::MoveRelative::BACKWARD);
+            move->setProperty("marker_ns", std::string("approach"));
+            const moveit::core::JointModelGroup* eef_jmg = t.getRobotModel()->getEndEffector(eef);
+            const auto& group_link = eef_jmg->getEndEffectorParentGroup();
+            move->setGroup(group_link.first);
+            move->setLink(group_link.second);
+            twist.header.frame_id = group_link.second;
+            move->along(twist);
+            move->setMinMaxDistance(0.05, 0.10);
+            merger->insert(std::unique_ptr<Stage>(move));
+        }
+        pick->insert(std::unique_ptr<Stage>(merger));
+    }
+
+    {  // bimanual grasp generator
+        auto gengrasp = new stages::BimanualGraspPose();
+        gengrasp->setMonitoredStage(referenced_stage);
+        gengrasp->setObject(object);
+        gengrasp->setEndEffectorPoses({{eef_left, "open"}, {eef_right, "open"}});
+
+        // inner IK: right hand
+        auto ik_inner = new stages::ComputeIK("compute ik right", std::unique_ptr<Stage>(gengrasp));
+        ik_inner->setEndEffector(eef_right);
+        ik_inner->properties().property("target_pose").configureInitFrom(Stage::INTERFACE, "target_pose_right");
+        ik_inner->setIKFrame(ik_frame_right);
+        ik_inner->setProperty("forward_properties", std::set<std::string>({"target_pose_left", "target_pose_right"}));
+
+        // outer IK: left hand
+        auto ik_outer = new stages::ComputeIK("compute ik left", std::unique_ptr<Stage>(ik_inner));
+        ik_outer->setEndEffector(eef_left);
+        ik_outer->properties().property("target_pose").configureInitFrom(Stage::INTERFACE, "target_pose_left");
+        ik_outer->setIKFrame(ik_frame_left);
+
+        pick->insert(std::unique_ptr<Stage>(ik_outer));
+    }
+
+    {  // allow touching the object
+        auto allow_touch = new stages::ModifyPlanningScene("allow object collision");
+        allow_touch->allowCollisions(object, t.getRobotModel()->getJointModelGroup(eef_left)->getLinkModelNamesWithCollisionGeometry(), true);
+        allow_touch->allowCollisions(object, t.getRobotModel()->getJointModelGroup(eef_right)->getLinkModelNamesWithCollisionGeometry(), true);
+        pick->insert(std::unique_ptr<Stage>(allow_touch));
+    }
+#if 1
+    {  // close hands
+        geometry_msgs::TwistStamped twist;
+        twist.twist.linear.y = 1.0;
+
+        auto merger = new Merger("close");
+        for (const auto& eef : {eef_left, eef_right}) {
+            auto move = new stages::MoveRelative("close " + eef, cartesian);
+            move->restrictDirection(stages::MoveRelative::FORWARD);
+            move->setProperty("marker_ns", std::string("close"));
+            const moveit::core::JointModelGroup* eef_jmg = t.getRobotModel()->getEndEffector(eef);
+            const auto& group_link = eef_jmg->getEndEffectorParentGroup();
+            move->setGroup(group_link.first);
+            move->setLink(group_link.second);
+            twist.header.frame_id = group_link.second;
+            move->along(twist);
+            move->setMinMaxDistance(0.02, 0.03);
+            merger->insert(std::unique_ptr<Stage>(move));
+        }
+        pick->insert(std::unique_ptr<Stage>(merger));
+    }
+#else
+    {  // close grippers
+        auto merger = new Merger("close grippers");
+        for (const auto& eef : {eef_left, eef_right}) {
+            auto move = new stages::MoveTo("close " + eef, pipeline);
+            move->restrictDirection(stages::MoveTo::FORWARD);
+            move->setGroup(t.getRobotModel()->getEndEffector(eef)->getName());
+            move->setGoal("close");
+            merger->insert(std::unique_ptr<Stage>(move));
+        }
+        pick->insert(std::unique_ptr<Stage>(merger));
+    }
+#endif
+    {  // attach object
+        auto attach = new stages::ModifyPlanningScene("attach object");
+        attach->attachObject(object, ik_frame_left.header.frame_id);
+        pick->insert(std::unique_ptr<Stage>(attach));
+    }
+
+    {  // lift
+        geometry_msgs::TwistStamped twist;
+        twist.twist.linear.z = 1.0;
+        twist.header.frame_id = "odom";
+
+        auto merger = new Merger("lift");
+        for (const auto& arm : {arm_left, arm_right}) {
+            auto move = new stages::MoveRelative("lift " + arm, cartesian);
+            move->restrictDirection(stages::MoveRelative::FORWARD);
+            move->setProperty("marker_ns", std::string("lift"));
+            move->setGroup(arm);
+            std::map<std::string, double> goal;
+            std::string shoulder_joint(1, std::toupper(arm[0]));
+            shoulder_joint.append("ShoulderPitch");
+            goal[shoulder_joint] = -0.25;
+            move->about(goal);
+            merger->insert(std::unique_ptr<Stage>(move));
+        }
+        pick->insert(std::unique_ptr<Stage>(merger));
+    }
+
+    t.add(std::unique_ptr<Stage>(pick));
+    return t;
+}
+
+
+
+/*
 void fillTask(Stage* initial_stage, std::string object_name) {
 
     // some robot specific variables
@@ -199,19 +371,23 @@ void fillTask(Stage* initial_stage, std::string object_name) {
     task->add(std::move(pick));
 
 }
-
+*/
 bool runTask(pepper_mtc_msgs::PepperGrasping::Request  &req,
              pepper_mtc_msgs::PepperGrasping::Response &res){
 
     // delete old task, create new one
-    task_ptr_.reset();
-    task_ptr_.reset(new Task());
+   // task_ptr_.reset();
+   // task_ptr_.reset(new Task());
+
+    Task t = createTask();
+
+    task_ptr_.reset(&t);
 
     auto task = task_ptr_.get();
 
     try {
         // start from the current state
-        Stage* initial_stage = nullptr;
+      /*  Stage* initial_stage = nullptr;
         auto initial = std::make_unique<stages::CurrentState>();
         initial_stage = initial.get();
 
@@ -219,6 +395,9 @@ bool runTask(pepper_mtc_msgs::PepperGrasping::Request  &req,
 
         // fill task with bidextral pipeline
         fillTask(initial_stage, req.object);
+        */
+
+
 
         // is blocking
         task->plan();
@@ -241,15 +420,19 @@ bool runTask(pepper_mtc_msgs::PepperGrasping::Request  &req,
         trajectory_msgs::JointTrajectory trajectory = sub_traj.trajectory.joint_trajectory;
         control_msgs::FollowJointTrajectoryGoal goal;
         goal.trajectory = trajectory;
-        action_client_ptr->sendGoal(goal);
-        bool finnished_before_timeout = action_client_ptr->waitForResult(ros::Duration(120.0));
+        if(trajectory.points.empty()){ // subsolution is not a trajectory (e.g. a planning-scene modification)
+            continue;
+        }
+        l_action_client_ptr->sendGoal(goal);
+        float timeout = 30.0;
+        bool finnished_before_timeout = l_action_client_ptr->waitForResult(ros::Duration(timeout));
         if(finnished_before_timeout){
-            actionlib::SimpleClientGoalState state = action_client_ptr->getState();
+            actionlib::SimpleClientGoalState state = l_action_client_ptr->getState();
             ROS_INFO("Action finished: %s",state.toString().c_str());
         }
         else{
-            ROS_INFO("Action did not finish before the time out. Cancelling pipeline");
-            action_client_ptr->cancelGoal();
+            ROS_INFO("Action did not finish before the time out.",std::to_string(timeout)," Cancelling pipeline");
+            l_action_client_ptr->cancelGoal();
             break;
         }
 
@@ -260,19 +443,23 @@ bool runTask(pepper_mtc_msgs::PepperGrasping::Request  &req,
     return true;
 }
 
+
 int main(int argc, char** argv){
     ros::init(argc, argv, "grasping");
 
     ros::NodeHandle n;
 
-    std::string action_topic = "/pepper_dcm/LeftArm_controller/follow_joint_trajectory";
+    std::string l_action_topic = "/pepper_dcm/LeftArm_controller/follow_joint_trajectory";
+    std::string r_action_topic = "/pepper_dcm/RightArm_controller/follow_joint_trajectory";
     // create the action client
     // true causes the client to spin its own thread
-    action_client_ptr.reset(new actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>(action_topic, true));
+    l_action_client_ptr.reset(new actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>(l_action_topic, true));
+    r_action_client_ptr.reset(new actionlib::SimpleActionClient<control_msgs::FollowJointTrajectoryAction>(r_action_topic, true));
 
     ROS_INFO("Waiting for action server to start.");
     // wait for the action server to start
-    action_client_ptr->waitForServer(); //will wait for infinite time
+    l_action_client_ptr->waitForServer(); //will wait for infinite time
+    r_action_client_ptr->waitForServer(); //will wait for infinite time
 
     ROS_INFO("action_server found.");
 
